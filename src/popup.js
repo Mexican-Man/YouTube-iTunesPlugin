abdb292f7d320f01d41d1f6e06c1fca51c532c27
Mexican-Man <colecrouter@hotmail.com>
2019-03-03 13:03:19 -0600

    Added all files

---
+++ src/popup.js
@@ -0,0 +1,50 @@
+var e = document.getElementById("country");
+var old = "";
+
+function createLinks(results) {
+	if (window.location.href.indexOf("youtube.com/watch")) {
+		// iTunes
+		if (results[0] !== 0) {
+			console.log("oof");
+			document.getElementById("apple").disabled = false;
+			document.getElementById("apple").addEventListener("click", function(){openLink(0)});
+		}
+		
+		/*// Spotify
+		if (results[1] !== 0) {
+			document.getElementById("spotify").disabled = false;
+			document.getElementById("spotify").addEventListener("click", function(){openLink(1)});
+		}*/
+	}
+}
+
+function openLink(num) {
+	switch (num) {
+		case 0:
+			chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+  				chrome.tabs.sendMessage(tabs[0].id, {text: "apple"}, function(response) {
+  				});
+			});
+			break;
+		case 1:
+			chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+  				chrome.tabs.sendMessage(tabs[0].id, {text: "spotify"}, function(response) {
+  				});
+			});
+			break;
+		case 2:
+			chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+  				chrome.tabs.sendMessage(tabs[0].id, {text: "google"}, function(response) {
+  				});
+			});
+	}
+}
+
+chrome.runtime.onMessage.addListener(function(message,sender,sendResponse){
+	createLinks(message.text);
+});
+
+chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+  chrome.tabs.sendMessage(tabs[0].id, {text: "trigger"}, function(response) {
+  });
+});
---
+++ src/script.js
@@ -0,0 +1,63 @@
+var results = ["","",""];
+var payload = [0,0,0];
+
+function filter(text) {
+	var ends = String("[](){}|").split("");
+	for (var i = 0; i < ends.length; i++) {
+		if (text.indexOf(ends[i]) >= 0) {
+			text = text.substr(0, text.indexOf(ends[i]));
+		}
+	}
+	return text.replace("-","").replace(".","").replace("ft", "");
+}
+
+function searchMusic() {
+	var filtered = filter(document.getElementsByTagName("h1")[0].innerHTML);
+	
+	// iTunes
+	var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("GET", ("https://itunes.apple.com/search?term=" + encodeURI(filtered)), false );
+	xmlHttp.send(null);
+	results[0] = JSON.parse(xmlHttp.responseText).results[0].trackViewUrl;
+	
+	/*// Spotify
+	var xmlHttp2 = new XMLHttpRequest();
+	//xmlHttp2.open("GET", "https://cors-anywhere.herokuapp.com/https://accounts.spotify.com/authorize?client_id=b943eb0ec8fc413783ec15b3b9f6f929&response_type=code&redirect_uri=http%3A%2F%2Fhoserswithzeuses.ca", false);
+	xmlHttp2.open("GET")
+	xmlHttp2.send(null);
+	console.log(xmlHttp2.responseText);
+	var code = JSON.parse(xmlHttp2.responseText).code;
+	
+	var xmlHttp3 = new XMLHttpRequest();
+	xmlHttp3.open("POST", "https://accounts.spotify.com/api/token", false);
+	xmlHttp3.send({"grant_type": "authorization_code", "code": code, "redirect_uri": "http://hoserswithzeuses.ca"});
+	var auth = JSON.parse(xmlHttp3.responseText).access_token;
+	console.log(auth);
+	*/
+	createPayload();
+	return results;
+	
+}
+
+function createPayload() {
+	for (var i = 0; i < 3; i++) {
+		if (results[i] !== "") {
+			payload[i] = 1;
+		}
+	}
+}
+
+chrome.runtime.onMessage.addListener(function(message,sender,sendResponse){
+    if (message.text === "trigger") {
+		searchMusic();
+        chrome.runtime.sendMessage({text: payload});
+    }
+	if (message.text.includes("apple")) {
+		document.getElementsByTagName("video")[0].pause();
+		its.detect.openItunes(results[0]);
+	}
+		/*if (message.text.includes("spotify")) {
+		console.log(results[0]);
+		its.detect.openItunes(results[0]);
+	}*/
+});
